e76f7da53f41b82ccb381b0d0d17b5c959e990b3
unknown <ricvera@favoritafc.com>
2026-03-09 21:56:23 -0500

    Check error infrastructure

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,19 +1,21 @@
 variable "location" {
-  type    = string
-  default = "East US"
+  description = "Azure region where resources will be created"
+  type        = string
+  default     = "East US"
 }
 
 variable "resource_group_name" {
-  type    = string
-  default = "rg-containerapps-demo"
+  description = "Resource Group for container apps"
+  type        = string
+  default     = "rg-containerapps-demo"
 }
 
 variable "backend_image" {
-  description = "Docker image for Spring Boot API"
+  description = "Docker image for the Spring Boot API backend"
   type        = string
 }
 
 variable "frontend_image" {
-  description = "Docker image for React frontend"
+  description = "Docker image for the React frontend"
   type        = string
 }
