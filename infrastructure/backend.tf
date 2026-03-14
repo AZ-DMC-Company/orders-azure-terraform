ffe0d76aa3c4e46b012b4179a702e92d309aa16d
unknown <ricvera@favoritafc.com>
2026-03-14 13:23:32 -0500

    Use hashicorp update values

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,27 +1,24 @@
 resource "azurerm_container_app" "backend" {
-  name                        = "${var.workload}-backend-${var.env}-eus-01"
+  name                     = "${var.workload}-backend-${var.env}-01"
   container_app_environment_id = azurerm_container_app_environment.cae.id
-  location                    = azurerm_resource_group.rg_app.location
-  resource_group_name         = azurerm_resource_group.rg_app.name
+  resource_group_name      = azurerm_resource_group.rg_app.name
+  location                 = azurerm_resource_group.rg_app.location
 
   revision_mode = "Single"
 
-  template {
-    container {
-      name   = "backend"
-      image  = var.backend_image
-      cpu    = 0.25
-      memory = "0.5Gi"
+  # Contenedor mínimo
+  container {
+    name   = "backend"
+    image  = var.backend_image
+    cpu    = 0.25
+    memory = "0.5Gi"
+    # Puerto de la app
+    target_port = 8080
+  }
 
-      env {
-        name  = "SPRING_PROFILES_ACTIVE"
-        value = var.env
-      }
-    }
-
-    scale {
-      min_replicas = 0
-      max_replicas = 1
-    }
+  # Public Access deshabilitado porque es privado
+  ingress {
+    external_enabled = false
+    target_port      = 8080
   }
 }
--- infrastructure/frontend.tf
+++ infrastructure/frontend.tf
@@ -1,27 +1,24 @@
 resource "azurerm_container_app" "frontend" {
-  name                        = "${var.workload}-frontend-${var.env}-eus-01"
+  name                     = "${var.workload}-frontend-${var.env}-01"
   container_app_environment_id = azurerm_container_app_environment.cae.id
-  location                    = azurerm_resource_group.rg_app.location
-  resource_group_name         = azurerm_resource_group.rg_app.name
+  resource_group_name      = azurerm_resource_group.rg_app.name
+  location                 = azurerm_resource_group.rg_app.location
 
   revision_mode = "Single"
 
-  template {
-    container {
-      name   = "frontend"
-      image  = var.frontend_image
-      cpu    = 0.25
-      memory = "0.5Gi"
-    }
-
-    scale {
-      min_replicas = 0
-      max_replicas = 1
-    }
+  # Contenedor mínimo
+  container {
+    name   = "frontend"
+    image  = var.frontend_image
+    cpu    = 0.25
+    memory = "0.5Gi"
+    target_port = 80
   }
 
-  traffic {
-    latest_revision = true
-    weight          = 100
+  # Public Access activado
+  ingress {
+    external_enabled = true
+    target_port      = 80
+    transport        = "Auto"
   }
 }
