3068a1ac1dafc776f8cc7a58901e94dcb74dabf5
unknown <ricvera@favoritafc.com>
2026-03-14 14:46:15 -0500

    Fix Terraformbackend

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,12 +1,13 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.69"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
+}
+terraform {
+  required_version = ">= 3.69"
+
+  backend "azurerm" {
+    resource_group_name  = "orders-rg-tfstate-dev-eus-01"
+    storage_account_name = "orderssttfdev01"
+    container_name       = "tfstate"
+    key                  = "app-infrastructure.tfstate"
+  }
 }
