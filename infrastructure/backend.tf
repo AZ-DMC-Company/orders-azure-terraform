421ff7d7f80c5a689e629398b1c7461d39316ead
unknown <ricvera@favoritafc.com>
2026-03-14 14:21:17 -0500

    Use hashicorp update values

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,14 +1,13 @@
 resource "azurerm_container_app" "backend" {
   name                         = "${var.workload}-backend-${var.env}-01"
   resource_group_name          = azurerm_resource_group.rg_app.name
-  location                     = azurerm_resource_group.rg_app.location
   container_app_environment_id = azurerm_container_app_environment.cae.id
   revision_mode                = "Single"
 
   ingress {
     external_enabled = false
     target_port      = 8080
-    transport        = "Auto"
+    transport        = "auto"
 
     traffic_weight {
       percentage    = 100
--- infrastructure/frontend.tf
+++ infrastructure/frontend.tf
@@ -1,14 +1,13 @@
 resource "azurerm_container_app" "frontend" {
   name                         = "${var.workload}-frontend-${var.env}-01"
   resource_group_name          = azurerm_resource_group.rg_app.name
-  location                     = azurerm_resource_group.rg_app.location
   container_app_environment_id = azurerm_container_app_environment.cae.id
   revision_mode                = "Single"
 
   ingress {
     external_enabled = true
     target_port      = 80
-    transport        = "Auto"
+    transport        = "auto"
 
     traffic_weight {
       percentage    = 100
