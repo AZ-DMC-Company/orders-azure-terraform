045223f74e1b92cabaf4e6588eb25a61e2926039
unknown <ricvera@favoritafc.com>
2026-03-14 13:28:55 -0500

    Use hashicorp update values

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,22 +1,25 @@
 resource "azurerm_container_app" "backend" {
-  name                           = "${var.workload}-backend-${var.env}-01"
-  resource_group_name            = azurerm_resource_group.rg_app.name
-  location                       = azurerm_resource_group.rg_app.location
-  container_app_environment_id   = azurerm_container_app_environment.cae.id
-  revision_mode                  = "Single"
+  name                         = "${var.workload}-backend-${var.env}-01"
+  resource_group_name          = azurerm_resource_group.rg_app.name
+  location                     = azurerm_resource_group.rg_app.location
+  container_app_environment_id = azurerm_container_app_environment.cae.id
+  revision_mode                = "Single"
 
   template {
     container {
-      name        = "backend"
-      image       = var.backend_image
-      cpu         = 0.25
-      memory      = "0.5Gi"
-      probes {
-        type     = "Liveness"
-        http_get {
-          path = "/health"
-          port = 8080
-        }
+      name   = "backend"
+      image  = var.backend_image
+      cpu    = 0.25
+      memory = "0.5Gi"
+
+      liveness_probe {
+        http_get_path = "/health"
+        http_get_port = 8080
+      }
+
+      readiness_probe {
+        http_get_path = "/health"
+        http_get_port = 8080
       }
     }
 
@@ -29,10 +32,5 @@ resource "azurerm_container_app" "backend" {
   ingress {
     external_enabled = false
     target_port      = 8080
-
-    traffic {
-      revision_name = "backend"
-      weight        = 100
-    }
   }
 }
--- infrastructure/frontend.tf
+++ infrastructure/frontend.tf
@@ -1,9 +1,9 @@
 resource "azurerm_container_app" "frontend" {
-  name                           = "${var.workload}-frontend-${var.env}-01"
-  resource_group_name            = azurerm_resource_group.rg_app.name
-  location                       = azurerm_resource_group.rg_app.location
-  container_app_environment_id   = azurerm_container_app_environment.cae.id
-  revision_mode                  = "Single"
+  name                         = "${var.workload}-frontend-${var.env}-01"
+  resource_group_name          = azurerm_resource_group.rg_app.name
+  location                     = azurerm_resource_group.rg_app.location
+  container_app_environment_id = azurerm_container_app_environment.cae.id
+  revision_mode                = "Single"
 
   template {
     container {
@@ -11,12 +11,15 @@ resource "azurerm_container_app" "frontend" {
       image  = var.frontend_image
       cpu    = 0.25
       memory = "0.5Gi"
-      probes {
-        type = "Liveness"
-        http_get {
-          path = "/"
-          port = 80
-        }
+
+      liveness_probe {
+        http_get_path = "/"
+        http_get_port = 80
+      }
+
+      readiness_probe {
+        http_get_path = "/"
+        http_get_port = 80
       }
     }
 
@@ -29,11 +32,5 @@ resource "azurerm_container_app" "frontend" {
   ingress {
     external_enabled = true
     target_port      = 80
-    transport        = "Auto"
-
-    traffic {
-      revision_name = "frontend"
-      weight        = 100
-    }
   }
 }
