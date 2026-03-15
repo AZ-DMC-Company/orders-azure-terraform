2c69b26c5e99568e058829069a4437d48399e14c
unknown <ricvera@favoritafc.com>
2026-03-15 13:35:41 -0500

    Allow all connections to the MySQL Server

--- infrastructure/mysql.tf
+++ infrastructure/mysql.tf
@@ -14,3 +14,13 @@ resource "azurerm_mysql_flexible_database" "grafana_db" {
   charset             = "utf8mb4"
   collation           = "utf8mb4_general_ci"
 }
+
+resource "azurerm_mysql_flexible_server_firewall_rule" "allow_all" {
+  name                = "allow-all"
+  resource_group_name = azurerm_resource_group.rg_app.name
+  server_name         = azurerm_mysql_flexible_server.mysql_server.name
+
+  # Para permitir cualquier IP
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "255.255.255.255"
+}
