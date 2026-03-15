851769c7bf11f32cf9461b1acb2afe518b4b4ffc
unknown <ricvera@favoritafc.com>
2026-03-15 11:03:11 -0500

    Change location

--- infrastructure/rg.tf
+++ infrastructure/rg.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "rg_app" {
-  name     = "${var.workload}-rg-app-${var.env}-eus-01"
+  name     = "${var.workload}-rg-app-${var.env}-${var.location}-01"
   location = var.location
 }
