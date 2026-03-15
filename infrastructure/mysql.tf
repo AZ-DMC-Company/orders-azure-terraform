4edb1e79975d081f0e67a6185459a377444e4af0
unknown <ricvera@favoritafc.com>
2026-03-15 00:51:48 -0500

    Use a no free MySQL

--- infrastructure/mysql.tf
+++ infrastructure/mysql.tf
@@ -1,7 +1,7 @@
 resource "azurerm_mysql_flexible_server" "grafana_db" {
   name                = "${var.workload}-grafana-db-${var.env}"
   resource_group_name = azurerm_resource_group.rg_app.name
-  location            = "westus2"
+  location            = azurerm_resource_group.rg_app.location
   administrator_login    = "grafanaadmin"
   administrator_password = var.grafana_mysql_password
   backup_retention_days  = 7
