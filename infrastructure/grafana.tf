6323756f0ab4783f91c4d264a99192c5b7bfb86b
unknown <ricvera@favoritafc.com>
2026-03-15 11:18:28 -0500

    Add grafana DB required

--- infrastructure/grafana.tf
+++ infrastructure/grafana.tf
@@ -53,10 +53,6 @@ resource "azurerm_container_app" "grafana" {
         name  = "GF_DATABASE_PASSWORD"
         value = var.grafana_mysql_password
       }
-      env {
-        name  = "GF_DATABASE_SSL_MODE"
-        value = "require"
-      }
     }
   }
 }
--- infrastructure/mysql.tf
+++ infrastructure/mysql.tf
@@ -7,3 +7,11 @@ resource "azurerm_mysql_flexible_server" "grafana_db" {
   backup_retention_days  = 7
   sku_name               = "B_Standard_B1ms"
 }
+
+resource "azurerm_mysql_flexible_server_database" "grafana_db" {
+  name                = "grafana"
+  resource_group_name = azurerm_resource_group.rg_app.name
+  server_name         = azurerm_mysql_flexible_server.grafana_db.name
+  charset             = "utf8mb4"
+  collation           = "utf8mb4_general_ci"
+}
