85d17f84fd92d8926d3a4b6dd3dc498246c2ce26
unknown <ricvera@favoritafc.com>
2026-03-14 20:59:14 -0500

    Fix attempt 1

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.5"
 
   backend "azurerm" {
-    resource_group_name  = "orders-rg-app-dev-eus-01"
+    resource_group_name  = "orders-rg-tfstate-dev-eus-01"
     storage_account_name = "orderssttfdev01"
     container_name       = "tfstate"
     key                  = "app-infrastructure.tfstate"
